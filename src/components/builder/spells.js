9dc47f783371dce51dc3b2b98831cdb239a33edf
Unknown <q.necron@gmail.com>
2022-01-22 20:14:40 -0600

    UI fixes for Spells and Bonus Spells
    Oops, conflicting classes.

--- src/components/builder/spells.js
+++ src/components/builder/spells.js
@@ -163,43 +163,43 @@ const BonusSpells = ({ ...props }) => {
     </div>
     <div className="spell-bonuses-container">
       <div className="spell-bonus">
-        <span className="spell-level">0th</span>
+        <span className="spell-bonus-level">0th</span>
         <div className="spell-dc">DC{10 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">1st</span> +{spl1} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">1st</span> +{spl1} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{11 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">2nd</span> +{spl2} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">2nd</span> +{spl2} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{12 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">3rd</span> +{spl3} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">3rd</span> +{spl3} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{13 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">4th</span> +{spl4} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">4th</span> +{spl4} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{14 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">5th</span> +{spl5} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">5th</span> +{spl5} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{15 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">6th</span> +{spl6} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">6th</span> +{spl6} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{16 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">7th</span> +{spl7} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">7th</span> +{spl7} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{17 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">8th</span> +{spl8} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">8th</span> +{spl8} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{18 + sdc}</div>
       </div>
       <div className="spell-bonus">
-        <span className="spell-level">9th</span> +{spl9} <span data-srt="true">bonus spells</span>
+        <span className="spell-bonus-level">9th</span> +{spl9} <span data-srt="true">bonus spells</span>
         <div className="spell-dc">DC{19 + sdc}</div>
       </div>
     </div>
